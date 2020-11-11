f9df89d69da3e91a7e0e3e0e3d90a2b8a72e74b6
David Dugle <david.dugleii@yahoo.com>
2020-11-11 13:03:59 -0500

    got it all working...yay

--- README.md
+++ README.md
@@ -1,5 +1,11 @@
 # Work-Day-Scheduler
 
+
+this was challenging, I needed a little help but proud of the fact that I didn't need as much as last time. I feel this is getting better but I do get stumped some times. I am new to coding and feel like I'm getting stronger.
+
+
+
+
 Making a work day scheduler that keeps up with the current time and shades the current hour and the upcoming hours differently. 
 The planner will pull the current day for use in the banner
 the planner will also store the saved data to the local storage.
@@ -7,6 +13,7 @@ got today's date to populate using JS
 the basic shell seems to be in good shape right now.
 oMG...using moment is so much better than what I was doing before. 
 Now have to get the transition fixed for the row colors, I am console logging the data attribute values. Now just need to add class.
+got the local storage working along with the color change 
 
 
 
--- assets/script.js
+++ assets/script.js
@@ -1,45 +1,51 @@
-var bodyEl = document.querySelector("#input9");
-
-
 
 
 // set the current day on the page
 $("#currentDay").text(moment().format("dddd,MMMM,Do"));
 
 //let's set a variable for the current hour so that we can use that to color code the rows
-var currentHour = moment().format("H");
+var currentHour = parseInt(moment().format("H"));
 console.log(currentHour)
 
 //now we are going to have to identify the current time of day and change the attibutes 
 var descripField = document.querySelectorAll("input.description");
-for (var i=0;i<descripField.length;i++){
-    dataHour = descripField[i].dataset.hour;
-    console.log(dataHour)
 
-    if (dataHour === currentHour){
-        $("descipField").addClass(".present");
-    };
-    // if else (dataHour > currentHour){
-    //     descipField.addClass(".future");
-    // };
-    // else if (dataHour<currentHour){
-    //     descipField.addClass(".past");
-    // };
-
+//this creates a loop to check the current hour all the way through the ending hour
+for (var i=9;i<18;i++){
+   
+  
+//this is checking the hour to see if it is the current hour and adding a class to change the color
+    if (i === currentHour){
+        $("#input"+i).addClass("present");
+    }
+    //this is checking the hour to see if it is in the future and adding the future class
+    else if (i > currentHour){
+        $("#input"+i).addClass("future");
+    }
+    //now we are setting the only other remaining class
+    else {
+        $("#input"+i).addClass("past");
+    }
+    //since we are checking the hour we are also going to check and see if there is anything in local storage that needs to go back
+    var localInfo = localStorage.getItem("input"+i)
+    $("#input"+i).val(localInfo)
+    
 };
 
 
-renderNine();
+
 //just need to set up local storage so that we can save and refresh the page without losing our data.
 
-$("#but9").on("click", function (event) {
+$(".saveBtn").on("click", function (event) {
     event.preventDefault();   
-    var entryForm9 = document.querySelector("#input9").value;   
-    localStorage.setItem("input9", entryForm9);
-    renderNine();    
-});
-function renderNine(){
-    var nineTalk = localStorage.getItem("input9");
-    document.querySelector("#input9").textContent= nineTalk;
+    //checking the value of the button that is clicked to reference the data-hour
+    var hour = $(this).attr("data-hour")
+    //checking the data that is in the clicked data-hour
+    var text = $("#input"+hour).val()
+    console.log(text)
 
-};
+    // var entryForm9 = document.querySelector("#input9").value;   
+    localStorage.setItem("input"+hour,text);
+    // renderNine();    
+});
+
